d3c738e7730f8150a1c555d2ab69095a36b440d8
Sukhmanjeet Singh <149586206+Sukhmanjeet1998@users.noreply.github.com>
2025-02-11 11:58:42 +0000

    Array

---
+++ 01_basics/09.1_arrays2.js
@@ -0,0 +1,19 @@
+let arr1 = [1,2,3,4,5];
+let arr2 = [6,7,8,9,10]
+
+// arr1.push(arr2);
+// console.log(arr1);
+
+// let arr3 = arr1.concat(arr2);
+// console.log(arr3);
+
+let arr4 = [...arr1,...arr2];
+// console.log(arr4);
+
+let arr5 = [1,2,3,[4,5,6],7,8,[9,10,[11,12,13,14,15]]];
+let arr6 = arr5.flat().flat();
+// console.log(arr6);
+
+let myName = "Sukhmanjeet Singh";
+let inArray = Array.from(myName);
+console.log(inArray);
--- 01_basics/09_arrays.js
+++ 01_basics/09_arrays.js
@@ -1,4 +1,4 @@
-const myArr = [26,null,true,999n,Symbol("@"),"Sunny",undefined];
+const myArr = [26,null,true,999n,"Sunny",undefined];
 
 // console.log(myArr);
 // console.log(typeof myArr[3], myArr[3]);
@@ -12,4 +12,26 @@ myArr.push(6);
 // console.log(myArr);
 
 myArr.unshift(0);
-console.log(myArr);
+// console.log(myArr);
+
+myArr.shift();
+// console.log(myArr);
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(4));
+
+let myNewArr = myArr.join();
+// console.log(myArr);
+// console.log(myNewArr);
+
+let arr1 = [0,1,2,3,4,5,6];
+// console.log("1: ",arr1);
+let arr2 = arr1.slice(0,3);
+// console.log("2: ",arr1);
+// console.log(arr2);
+
+let arr3 = arr1.splice(0,3);
+
+// console.log("3: ",arr1);
+// console.log(arr3);
+
