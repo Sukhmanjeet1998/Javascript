ca6d454cac6da271883e958fa0745f99c0e7c962
Sukhmanjeet Singh <149586206+Sukhmanjeet1998@users.noreply.github.com>
2025-02-14 12:32:52 +0000

    IIFE

--- 01_basics/15_IIFE.js
+++ 01_basics/15_IIFE.js
@@ -3,32 +3,49 @@
 // Regular function
 // function chai(){
 //     console.log("DB connected");
-    
+
 // }
 // chai();
 
 // (function defination)(function call)
 
 // named iife
-(function chai(){
+(function chai() {
     console.log("DB connected");
-    
+
 })();
 
 // without name iife
-(()=>{
+(() => {
     console.log("DB connected");
 })();
 
 // passing parameter to the iife
-((num1,num2)=>{
-    console.log(num1+num2);
-})(5,6);
+((num1, num2) => {
+    console.log(num1 + num2);
+})(5, 6);
 
-((databaseName)=>{
+((databaseName) => {
     console.log(`My database name is ${databaseName}`);
 })("Amazon");
 
 
 
 // Why IIFE: to prevent from global pollution
+
+/*
+
+Why Use IIFE?
+-Avoid Global Namespace Pollution
+Variables inside an IIFE are not accessible globally, preventing conflicts with other scripts.
+
+-Encapsulation & Data Privacy
+Helps in creating private variables that are not accessible outside the function.
+
+-Avoid Hoisting Issues
+Since IIFE executes immediately, there’s no risk of variable hoisting affecting the logic.
+
+-Immediate Execution
+Useful for executing code immediately without waiting for function calls.
+
+*/
